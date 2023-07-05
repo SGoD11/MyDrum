681710b95d54c946ea4f3516039f56ce450617bc
Subhajit_Dhar <103268215+SGoD11@users.noreply.github.com>
2023-07-05 09:38:36 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -47,30 +47,44 @@ document.addEventListener("keypress", function (event) {
 function makeSound(key) {
     switch (key) {
         case "w":
+            document.querySelector("h1").style.color="green";
+            setTimeout(function(){document.querySelector("h1").style.color="#DBEDF3"},300);
             var tom1 = new Audio('./sounds/tom-1.mp3');
             tom1.play();
             break;
         case "a":
+            document.querySelector("h1").style.color="red";
+            setTimeout(function(){document.querySelector("h1").style.color="#DBEDF3"},300);
             var tom2 = new Audio('./sounds/tom-2.mp3');
             tom2.play();
             break;
         case "s":
+            document.querySelector("h1").style.color="pink";
+            setTimeout(function(){document.querySelector("h1").style.color="#DBEDF3"},300);
             var tom3 = new Audio('./sounds/tom-3.mp3');
             tom3.play();
             break;
         case "d":
+            document.querySelector("h1").style.color="purple";
+            setTimeout(function(){document.querySelector("h1").style.color="#DBEDF3"},300);
             var tom4 = new Audio('./sounds/tom-4.mp3');
             tom4.play();
             break;
         case "j":
+            document.querySelector("h1").style.color="orange";
+            setTimeout(function(){document.querySelector("h1").style.color="#DBEDF3"},300);
             var snare = new Audio('./sounds/snare.mp3');
             snare.play();
             break;
         case "k":
+            document.querySelector("h1").style.color="blue";
+            setTimeout(function(){document.querySelector("h1").style.color="#DBEDF3"},300);
             var crash = new Audio('./sounds/crash.mp3');
             crash.play();
             break;
         case "l":
+            document.querySelector("h1").style.color="yellow";
+            setTimeout(function(){document.querySelector("h1").style.color="#DBEDF3"},300);
             var kickBass = new Audio('./sounds/kick-bass.mp3');
             kickBass.play();
             break;
